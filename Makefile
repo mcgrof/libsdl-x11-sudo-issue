6e695521eb8449d741b849b85f81f61a77a071e8
Luis R. Rodriguez <mcgrof@suse.com>
2014-03-20 14:52:47 -0700

    libsdl-x11-sudo-issue: rewrite in C
    C++ complexity is not needed for this simple example.
    
    Signed-off-by: Luis R. Rodriguez <mcgrof@suse.com>

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: sdl1 sdl2
 
 sdl2: sdl.c
-	g++ -lSDL -o sdl2 sdl.c
+	gcc -lSDL -o sdl2 sdl.c
 
 sdl1: sdl.c
-	g++ -lSDL -o sdl1 sdl.c
+	gcc -lSDL -o sdl1 sdl.c
 
 clean:
 	rm -f sdl1 sdl2
--- sdl.c
+++ sdl.c
@@ -1,75 +1,25 @@
-#include <exception>
-#include <string>
-#include <SDL/SDL.h>
+#include <stdio.h>
 #include <stdlib.h>
-
-class InitError:public std::exception {
-public:
-	InitError();
-	InitError(const std::string &);
-	virtual ~ InitError() throw();
-	virtual const char *what() const throw();
-private:
-	std::string msg;
-};
-
-InitError::InitError():
-exception(), msg(SDL_GetError())
-{
-}
-
-InitError::InitError(const std::string & m):exception(), msg(m)
-{
-}
-
-InitError::~InitError()throw()
-{
-}
-
-const char *InitError::what() const throw()
-{
-	return msg.c_str();
-}
-
-class SDL {
-public:
-	SDL(Uint32 flags = 0) throw(InitError);
-	virtual ~ SDL();
-};
-
-SDL::SDL(Uint32 flags) throw(InitError)
-{
-	if (SDL_Init(flags) != 0)
-		throw InitError();
-}
-
-SDL::~SDL()
-{
-	SDL_Quit();
-}
-
-/* ... */
-
-#include <iostream>
+#include <SDL/SDL.h>
 
 int main(int argc, char **argv)
 {
+	int r, flags;
+
 	setenv("SDL_VIDEO_ALLOW_SCREENSAVER", "1", 0);
 	setenv("SDL_VIDEODRIVER", "x11", 0);
 	setenv("SDL_DISABLE_LOCK_KEYS", "1", 1);
-	try {
-		SDL sdl(SDL_INIT_VIDEO | SDL_INIT_TIMER |
-			SDL_INIT_NOPARACHUTE);
 
-		/* ... */
+	flags = SDL_INIT_VIDEO |
+		SDL_INIT_TIMER |
+		SDL_INIT_NOPARACHUTE;
 
-		return 0;
+	r = SDL_Init(flags);
+	if (r) {
+		fprintf(stderr, "Could not initialize SDL(%s) - exiting\n",
+			SDL_GetError());
+		exit(r);
 	}
 
-	catch(const InitError & err) {
-		std::cerr << "Error while initializing SDL:  " << err.
-		    what() << std::endl;
-	}
-
-	return 1;
+	return 0;
 }
